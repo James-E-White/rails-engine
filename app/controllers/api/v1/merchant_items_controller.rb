f8ac75ed82a8eea63d92d4892706cdbf365e6abc
James White <whitejaad@gmail.com>
2022-12-07 13:43:04 -0800

    created an item

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -4,3 +4,13 @@ class Api::V1::MerchantItemsController < ApplicationController
     render json: ItemSerializer.new(merchant.items)
   end
 end
+
+
+# def show 
+#         # require 'pry';binding.pry
+#         if Market.exists?(params[:id])
+#             render json: MarketSerializer.new(market)
+#         else 
+#             render json: {"errors": "Market with this id doesn't exist"} , status: 404
+#         end 
+#     end
